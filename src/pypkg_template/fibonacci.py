68ea92d4786f4448fdeab03b91583eb75b6f714e
Sora Shiratani <hclfuranpon_compotayamano_0117@outlook.jp>
2023-07-03 03:22:07 +0900

    :construction: First impl.

---
+++ src/pypkg_template/fibonacci.py
@@ -0,0 +1,65 @@
+"""Various ways to calculate the Fibonacci sequence."""
+
+
+from collections.abc import Callable
+
+
+def _argcheck(n: int) -> None:
+    if n < 0:
+        msg = f"n must be >=0, got {n}."
+        raise ValueError(msg)
+
+
+def naive(n: int) -> int:
+    """Calculate the nth Fibonacci number using a naive recursive algorithm.
+
+    Parameters
+    ----------
+    n : int
+        The index of the Fibonacci number to calculate.
+    """
+    _argcheck(n)
+    if n in (0, 1):
+        return n
+    return naive(n - 1) + naive(n - 2)
+
+
+def closuredp() -> Callable[[int], int]:
+    """Return a closure that calculates the nth Fibonacci number."""
+    mem = {0: 0, 1: 1}
+
+    def fib_closure(n: int) -> int:
+        """Calculate the nth Fibonacci number. Results are memoized.
+
+        Parameters
+        ----------
+        n : int
+            The index of the Fibonacci number to calculate.
+        """
+        _argcheck(n)
+        if n not in mem:
+            mem[n] = fib_closure(n - 1) + fib_closure(n - 2)
+        return mem[n]
+
+    return fib_closure
+
+
+class ClassDP:
+    """A callable class internally memoizing the Fibonacci sequence."""
+
+    def __init__(self) -> None:
+        """Initialize ClassDP."""
+        self.__mem = {0: 0, 1: 1}
+
+    def __call__(self, n: int) -> int:
+        """Calculate the nth Fibonacci number. Results are memoized.
+
+        Parameters
+        ----------
+        n : int
+            The index of the Fibonacci number to calculate.
+        """
+        _argcheck(n)
+        if n not in self.__mem:
+            self.__mem[n] = self(n - 1) + self(n - 2)
+        return self.__mem[n]
