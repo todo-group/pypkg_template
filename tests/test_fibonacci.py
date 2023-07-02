e68bf9b5aa0754d355cb485dca708be9d8a7d57d
S.S. <66886825+EarlMilktea@users.noreply.github.com>
2023-07-03 03:22:27 +0900

    :white_check_mark: Add tests

---
+++ tests/test_fibonacci.py
@@ -0,0 +1,27 @@
+"""Test pypkg_template.fibonacci module."""
+
+from pypkg_template import fibonacci
+
+_NAIVE_TOL: int = 30
+
+
+def test_naive(fx_exact: dict[int, int]) -> None:
+    """Test naive."""
+    for n, ref in fx_exact.items():
+        if n > _NAIVE_TOL:
+            continue
+        assert fibonacci.naive(n) == ref
+
+
+def test_closuredp(fx_exact: dict[int, int]) -> None:
+    """Test closuredp."""
+    fib = fibonacci.closuredp()
+    for n, ref in fx_exact.items():
+        assert fib(n) == ref
+
+
+def test_classdp(fx_exact: dict[int, int]) -> None:
+    """Test ClassDP."""
+    fib = fibonacci.ClassDP()
+    for n, ref in fx_exact.items():
+        assert fib(n) == ref
